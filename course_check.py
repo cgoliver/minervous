b5bc68e18254cebf2a9a9f652d8747fccf4474c4
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2017-11-16 16:29:57 -0500

    got to course page

--- course_check.py
+++ course_check.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import os  
 import urllib
 import requests
@@ -6,42 +7,85 @@ import requests
 from selenium import webdriver  
 from selenium.webdriver.common.keys import Keys  
 from selenium.webdriver.chrome.options import Options
-
-
-chrome_options = Options()  
-chrome_options.add_argument("--headless")  
-chrome_options.binary_location = '/Applications/Google Chrome \
-    Canary.app/Contents/MacOS/Google Chrome Canary' 
-
-
-driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),\
-    chrome_options=chrome_options)
+from selenium.webdriver.support.ui import Select
 
 search_url = "https://horizon.mcgill.ca/pban1/bwskfcls.P_GetCrse"
 login_url = "https://horizon.mcgill.ca/pban1/twbkwbis.P_ValLogin"
 
-username = "carlos.gonzalezoliver@mail.mcgill.ca"
+uname = "carlos.gonzalezoliver@mail.mcgill.ca"
 pwd = "sasjajuli_milojuli"
 
-params=(("sid",username),
-        ("PIN",pwd))
+chrome_options = Options()
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--window-size=1920x1080")
 
-data=urllib.parse.urlencode(params)
 
-cookies={"TESTID":"set"}
+driver = webdriver.Chrome(chrome_options=chrome_options,\
+    executable_path=os.path.abspath("chromedriver"))
 
-r_login = requests.post(login_url,data=data,cookies=cookies)
-sessionid = r_login.cookies.get("SESSID")
+def login(username, password):
+    #get login page
+    driver.get(login_url)
+    time.sleep(5)
+    #give email
+    uname = driver.find_element_by_xpath("//*[@id=\"mcg_un\"]")
 
-print(sessionid)
+    #give password
+    pw = driver.find_element_by_xpath("//*[@id=\"mcg_pw\"]")
 
-r=requests.post(search_url,cookies={"SESSID": sessionid})
-print(r.status_code)
-print(r.url)
+    uname.send_keys(username)
+    pw.send_keys(password)
 
-driver.get(r.url)
+    time.sleep(1)
 
-time.sleep(5)
+    submit = driver.find_element_by_xpath("//*[@id=\"mcg_un_submit\"]")
 
-htmlSource = driver.page_source
-printt(htmlSource)
+    submit.click()
+
+    return driver.page_source 
+
+def course_search(deptartment, course, term="Winter 2018", dept="COMP"):
+    driver.get("https://horizon.mcgill.ca/pban1/bwskfcls.p_sel_crse_search")
+    select = Select(driver.find_element_by_id("term_input_id"))
+    select.select_by_visible_text(term)
+    time.sleep(2)
+    # print(driver.page_source)
+    print(driver.current_url)
+
+    driver.find_element_by_xpath("/html/body/div[3]/form/input[3]").click()
+
+    time.sleep(2)
+    sel = driver.find_elements_by_name("sel_subj")[1]
+    select = Select(sel)
+    select.select_by_value(dept)
+        
+    
+    #submit department
+    driver.find_element_by_name("SUB_BTN").click()
+    time.sleep(2)
+    print(driver.current_url)
+
+    #select course
+    rows = driver.find_elements_by_tag_name("tr")
+    for row in rows:
+        try:
+            course_num = row.text.split()[0]
+            print(course_num)
+        except IndexError:
+            continue
+        else:
+            if course_num == "767":
+                print(course)
+                row.find_element_by_name("SUB_BTN").click()
+                break
+
+    # course = driver.find_element_by_xpath("//input[@name='SEL_CRSE' and\
+        # @value='767']").click()
+    
+    # driver.find_element_by_link_text("Student Menu").click()
+    # driver.find_element_by_link_text("Registration Menu").click()
+    
+login(uname, pwd)
+course_search("hi", "ho")
+# htmlSource = driver.page_source
+# printt(htmlSource)
